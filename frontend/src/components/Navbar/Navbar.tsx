057c385416545d59303482d5141ceb3e9c66c982
Mohit Aasirwal <mohitaasirwal@gmail.com>
2024-03-05 16:46:28 +0530

    landing page done

--- frontend/src/components/Navbar/Navbar.tsx
+++ frontend/src/components/Navbar/Navbar.tsx
@@ -1,10 +1,11 @@
 import React from "react";
+import Connect from "../Buttons/Connect";
 
 const Navbar = () => {
   return (
     <div className="flex flex-row justify-between items-center">
       <h1 className="font-bold text-3xl font-[Poppins]">Finsure</h1>
-      <button>demo</button>
+      <Connect />
     </div>
   );
 };
--- frontend/src/containers/Landing/Landing.tsx
+++ frontend/src/containers/Landing/Landing.tsx
@@ -1,3 +1,4 @@
+import Image from "next/image";
 import React from "react";
 
 const Landing = () => {
@@ -7,18 +8,31 @@ const Landing = () => {
         <span>Get Rid of delaying flights,</span>{" "}
         <span>get your precious time back monetarily</span>
       </h1>
-      <div className="w-1/2 p-5 bg-[#20262E] h-full flex flex-col space-y-5 rounded-md">
-        <h1 className="text-2xl font-light">
-          Travel anywhere across the globe
-        </h1>
-        <p className="font-extralight text-md">
-          Insurance plan starting at just ₹199
-        </p>
-        <input
-          type="text"
-          placeholder="Enter the PNR Number here..."
-          className="p-2 rounded-xl bg-[#2F363E] placeholder-[#8C8C8C6E]"
-        />
+      <div className="w-1/2 p-10 bg-[#20262E] h-full flex flex-col space-y-8 rounded-md">
+        <div className="flex flex-col space-y-2">
+          <h1 className="text-2xl font-light">
+            Travel anywhere across the globe
+          </h1>
+          <p className="font-extralight text-md">
+            Insurance plan starting at just ₹199
+          </p>
+        </div>
+        <div className="w-full flex flex-row space-x-2">
+          <input
+            type="text"
+            placeholder="Enter the PNR Number here..."
+            className="p-2 rounded-xl w-full bg-[#2F363E] placeholder-[#8C8C8C6E]"
+          />
+          <button className="bg-[#132C51] px-6 py-1 border border-[#285FB3] rounded-md">
+            <Image
+              src={"/arrow.svg"}
+              className="w-5 h-full"
+              alt=""
+              width={100}
+              height={100}
+            />
+          </button>
+        </div>
       </div>
     </div>
   );
