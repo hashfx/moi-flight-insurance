782c5c4da3325b5ed17323ca888da23aa9565ad7
Mohit Aasirwal <mohitaasirwal@gmail.com>
2024-03-05 16:25:38 +0530

    files mustafa made

---
+++ frontend/src/components/Buttons/Connect.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Connect = () => {
+	return (
+		<button className="w-button p-button bg-Primary border border-Secondary rounded-lg">
+			Connect
+		</button>
+	);
+};
+
+export default Connect;
