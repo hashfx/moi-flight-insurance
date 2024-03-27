27e8403804eeff112576529fbdb67a8051e4204f
Mustafa Azad <mustafaazad533@gmail.com>
2024-03-27 21:12:41 +0530

    minor changes

--- frontend/src/components/DetailForm/DetailForm.tsx
+++ frontend/src/components/DetailForm/DetailForm.tsx
@@ -1,13 +1,11 @@
-import { PhotoIcon, UserCircleIcon } from '@heroicons/react/24/solid'
-
 export default function DetailForm() {
   return (
-    <form className='mt-16 p-8'>
+    <form className='p-8'>
       <div className="space-y-12 mt-10">
 
         <div className="border-b border-white/10 pb-12">
-          <h2 className="text-base font-semibold leading-7 text-white">Personal Information</h2>
-          <p className="mt-1 text-sm leading-6 text-gray-400">Use a permanent address where you can receive mail.</p>
+          <h2 className="text-base font-semibold leading-7 text-white">Flight Information</h2>
+          <p className="mt-1 text-sm leading-6 text-gray-400">Please fill correct information.</p>
 
           <div className="mt-10 grid grid-cols-1 gap-x-6 gap-y-8 sm:grid-cols-6">
             <div className="sm:col-span-3">
@@ -41,15 +39,15 @@ export default function DetailForm() {
             </div>
 
             <div className="sm:col-span-4">
-              <label htmlFor="email" className="block text-sm font-medium leading-6 text-white">
-                Email address
+              <label htmlFor="departure-date" className="block text-sm font-medium leading-6 text-white">
+                Departure Date
               </label>
               <div className="mt-2">
                 <input
-                  id="email"
-                  name="email"
-                  type="email"
-                  autoComplete="email"
+                  id="departure-date"
+                  name="departure-date"
+                  type="datetime-local"
+                  autoComplete="departure-date"
                   className="block w-full rounded-md border-0 bg-white/5 py-1.5 text-white shadow-sm ring-1 ring-inset ring-white/10 focus:ring-2 focus:ring-inset focus:ring-indigo-500 sm:text-sm sm:leading-6"
                 />
               </div>
@@ -66,68 +64,71 @@ export default function DetailForm() {
                   autoComplete="country-name"
                   className="block w-full rounded-md border-0 bg-white/5 py-1.5 text-white shadow-sm ring-1 ring-inset ring-white/10 focus:ring-2 focus:ring-inset focus:ring-indigo-500 sm:text-sm sm:leading-6 [&_*]:text-black"
                 >
-                  <option>United States</option>
-                  <option>Canada</option>
-                  <option>Mexico</option>
+                  <option className="bg-white/5">India</option>
+                  <option className="bg-white/5">United States</option>
+                  <option className="bg-white/5">Mexico</option>
                 </select>
               </div>
             </div>
 
             <div className="col-span-full">
-              <label htmlFor="street-address" className="block text-sm font-medium leading-6 text-white">
-                Street address
+              <label htmlFor="flight-from" className="block text-sm font-medium leading-6 text-white">
+                Flight Source
               </label>
               <div className="mt-2">
                 <input
                   type="text"
-                  name="street-address"
-                  id="street-address"
-                  autoComplete="street-address"
+                  name="flight-from"
+                  id="flight-from"
+                  autoComplete="flight-from"
+                  placeholder="e.g Indore, Bhopal, Mumbai ..."
                   className="block w-full rounded-md border-0 bg-white/5 py-1.5 text-white shadow-sm ring-1 ring-inset ring-white/10 focus:ring-2 focus:ring-inset focus:ring-indigo-500 sm:text-sm sm:leading-6"
                 />
               </div>
             </div>
 
             <div className="sm:col-span-2 sm:col-start-1">
-              <label htmlFor="city" className="block text-sm font-medium leading-6 text-white">
-                City
+              <label htmlFor="flight-to" className="block text-sm font-medium leading-6 text-white">
+                Flight Destination
               </label>
               <div className="mt-2">
                 <input
                   type="text"
-                  name="city"
-                  id="city"
-                  autoComplete="address-level2"
+                  name="flight-to"
+                  id="flight-to"
+                  autoComplete="flight-to"
+                  placeholder="e.g Indore, Bhopal, Mumbai ..."
                   className="block w-full rounded-md border-0 bg-white/5 py-1.5 text-white shadow-sm ring-1 ring-inset ring-white/10 focus:ring-2 focus:ring-inset focus:ring-indigo-500 sm:text-sm sm:leading-6"
                 />
               </div>
             </div>
 
             <div className="sm:col-span-2">
-              <label htmlFor="region" className="block text-sm font-medium leading-6 text-white">
-                State / Province
+              <label htmlFor="flight-number" className="block text-sm font-medium leading-6 text-white">
+                Flight Number
               </label>
               <div className="mt-2">
                 <input
                   type="text"
-                  name="region"
-                  id="region"
+                  name="flight-number"
+                  id="flight-number"
                   autoComplete="address-level1"
+                  placeholder="e.g AI-1234"
                   className="block w-full rounded-md border-0 bg-white/5 py-1.5 text-white shadow-sm ring-1 ring-inset ring-white/10 focus:ring-2 focus:ring-inset focus:ring-indigo-500 sm:text-sm sm:leading-6"
                 />
               </div>
             </div>
 
             <div className="sm:col-span-2">
-              <label htmlFor="postal-code" className="block text-sm font-medium leading-6 text-white">
-                ZIP / Postal code
+              <label htmlFor="seat" className="block text-sm font-medium leading-6 text-white">
+                Seat Number
               </label>
               <div className="mt-2">
                 <input
                   type="text"
-                  name="postal-code"
-                  id="postal-code"
-                  autoComplete="postal-code"
+                  name="seat"
+                  id="seat"
+                  autoComplete="seat"
                   className="block w-full rounded-md border-0 bg-white/5 py-1.5 text-white shadow-sm ring-1 ring-inset ring-white/10 focus:ring-2 focus:ring-inset focus:ring-indigo-500 sm:text-sm sm:leading-6"
                 />
               </div>
