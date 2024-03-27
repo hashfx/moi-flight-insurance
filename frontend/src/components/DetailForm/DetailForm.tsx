de6789bd808d40765363f651186ce8f5727c8a9a
Mustafa Azad <mustafaazad533@gmail.com>
2024-03-27 22:29:12 +0530

    fixed issue

--- frontend/src/components/DetailForm/DetailForm.tsx
+++ frontend/src/components/DetailForm/DetailForm.tsx
@@ -69,8 +69,8 @@ export default function DetailForm() {
                   <option className="bg-white/5">Mexico</option>
                 </select>
               </div>
+              </div>
 
-<<<<<<< HEAD
             <div className="col-span-full">
               <label htmlFor="flight-from" className="block text-sm font-medium leading-6 text-white">
                 Flight Source
@@ -84,27 +84,9 @@ export default function DetailForm() {
                   placeholder="e.g Indore, Bhopal, Mumbai ..."
                   className="block w-full rounded-md border-0 bg-white/5 py-1.5 text-white shadow-sm ring-1 ring-inset ring-white/10 focus:ring-2 focus:ring-inset focus:ring-indigo-500 sm:text-sm sm:leading-6"
                 />
-=======
-              <div className="sm:col-span-3">
-                <label
-                  htmlFor="last-name"
-                  className="block text-sm font-medium leading-6 text-white"
-                >
-                  Last name
-                </label>
-                <div className="mt-2">
-                  <input
-                    type="text"
-                    name="last-name"
-                    id="last-name"
-                    autoComplete="family-name"
-                    className="block w-full rounded-md border-0 bg-white/5 py-1.5 text-white shadow-sm ring-1 ring-inset ring-white/10 focus:ring-2 focus:ring-inset focus:ring-indigo-500 sm:text-sm sm:leading-6"
-                  />
-                </div>
->>>>>>> 5149d1325ff5741f52bfbf04bfddb274c5392946
+              </div>
               </div>
 
-<<<<<<< HEAD
             <div className="sm:col-span-2 sm:col-start-1">
               <label htmlFor="flight-to" className="block text-sm font-medium leading-6 text-white">
                 Flight Destination
@@ -118,27 +100,8 @@ export default function DetailForm() {
                   placeholder="e.g Indore, Bhopal, Mumbai ..."
                   className="block w-full rounded-md border-0 bg-white/5 py-1.5 text-white shadow-sm ring-1 ring-inset ring-white/10 focus:ring-2 focus:ring-inset focus:ring-indigo-500 sm:text-sm sm:leading-6"
                 />
-=======
-              <div className="sm:col-span-4">
-                <label
-                  htmlFor="email"
-                  className="block text-sm font-medium leading-6 text-white"
-                >
-                  Email address
-                </label>
-                <div className="mt-2">
-                  <input
-                    id="email"
-                    name="email"
-                    type="email"
-                    autoComplete="email"
-                    className="block w-full rounded-md border-0 bg-white/5 py-1.5 text-white shadow-sm ring-1 ring-inset ring-white/10 focus:ring-2 focus:ring-inset focus:ring-indigo-500 sm:text-sm sm:leading-6"
-                  />
-                </div>
->>>>>>> 5149d1325ff5741f52bfbf04bfddb274c5392946
               </div>
 
-<<<<<<< HEAD
             <div className="sm:col-span-2">
               <label htmlFor="flight-number" className="block text-sm font-medium leading-6 text-white">
                 Flight Number
@@ -152,34 +115,8 @@ export default function DetailForm() {
                   placeholder="e.g AI-1234"
                   className="block w-full rounded-md border-0 bg-white/5 py-1.5 text-white shadow-sm ring-1 ring-inset ring-white/10 focus:ring-2 focus:ring-inset focus:ring-indigo-500 sm:text-sm sm:leading-6"
                 />
-=======
-              <div className="sm:col-span-3">
-                <label
-                  htmlFor="country"
-                  className="block text-sm font-medium leading-6 text-white"
-                >
-                  Country
-                </label>
-                <div className="mt-2">
-                  <select
-                    id="country"
-                    name="country"
-                    autoComplete="country-name"
-                    className="block w-full rounded-md border-0 bg-white/5 py-1.5 text-white shadow-sm ring-1 ring-inset ring-white/10 focus:ring-2 focus:ring-inset focus:ring-indigo-500 sm:text-sm sm:leading-6 [&_*]:text-black"
-                  >
-                    {countries.features.map((singleCountry, index) => {
-                      return (
-                        <option key={index} className="">
-                          {singleCountry.properties.geounit}
-                        </option>
-                      );
-                    })}
-                  </select>
-                </div>
->>>>>>> 5149d1325ff5741f52bfbf04bfddb274c5392946
               </div>
 
-<<<<<<< HEAD
             <div className="sm:col-span-2">
               <label htmlFor="seat" className="block text-sm font-medium leading-6 text-white">
                 Seat Number
@@ -192,78 +129,6 @@ export default function DetailForm() {
                   autoComplete="seat"
                   className="block w-full rounded-md border-0 bg-white/5 py-1.5 text-white shadow-sm ring-1 ring-inset ring-white/10 focus:ring-2 focus:ring-inset focus:ring-indigo-500 sm:text-sm sm:leading-6"
                 />
-=======
-              <div className="col-span-full">
-                <label
-                  htmlFor="street-address"
-                  className="block text-sm font-medium leading-6 text-white"
-                >
-                  Street address
-                </label>
-                <div className="mt-2">
-                  <input
-                    type="text"
-                    name="street-address"
-                    id="street-address"
-                    autoComplete="street-address"
-                    className="block w-full rounded-md border-0 bg-white/5 py-1.5 text-white shadow-sm ring-1 ring-inset ring-white/10 focus:ring-2 focus:ring-inset focus:ring-indigo-500 sm:text-sm sm:leading-6"
-                  />
-                </div>
-              </div>
-
-              <div className="sm:col-span-2 sm:col-start-1">
-                <label
-                  htmlFor="city"
-                  className="block text-sm font-medium leading-6 text-white"
-                >
-                  City
-                </label>
-                <div className="mt-2">
-                  <input
-                    type="text"
-                    name="city"
-                    id="city"
-                    autoComplete="address-level2"
-                    className="block w-full rounded-md border-0 bg-white/5 py-1.5 text-white shadow-sm ring-1 ring-inset ring-white/10 focus:ring-2 focus:ring-inset focus:ring-indigo-500 sm:text-sm sm:leading-6"
-                  />
-                </div>
-              </div>
-
-              <div className="sm:col-span-2">
-                <label
-                  htmlFor="region"
-                  className="block text-sm font-medium leading-6 text-white"
-                >
-                  State / Province
-                </label>
-                <div className="mt-2">
-                  <input
-                    type="text"
-                    name="region"
-                    id="region"
-                    autoComplete="address-level1"
-                    className="block w-full rounded-md border-0 bg-white/5 py-1.5 text-white shadow-sm ring-1 ring-inset ring-white/10 focus:ring-2 focus:ring-inset focus:ring-indigo-500 sm:text-sm sm:leading-6"
-                  />
-                </div>
-              </div>
-
-              <div className="sm:col-span-2">
-                <label
-                  htmlFor="postal-code"
-                  className="block text-sm font-medium leading-6 text-white"
-                >
-                  ZIP / Postal code
-                </label>
-                <div className="mt-2">
-                  <input
-                    type="text"
-                    name="postal-code"
-                    id="postal-code"
-                    autoComplete="postal-code"
-                    className="block w-full rounded-md border-0 bg-white/5 py-1.5 text-white shadow-sm ring-1 ring-inset ring-white/10 focus:ring-2 focus:ring-inset focus:ring-indigo-500 sm:text-sm sm:leading-6"
-                  />
-                </div>
->>>>>>> 5149d1325ff5741f52bfbf04bfddb274c5392946
               </div>
             </div>
           </div>
@@ -283,7 +148,9 @@ export default function DetailForm() {
             Save
           </button>
         </div>
+      </div>
+      </div>
+      </div>
       </form>
-    </div>
   );
 }
